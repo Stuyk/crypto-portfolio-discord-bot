41328f4946794f3808909e15ab6ac393f942f758
Tim <chipsleten@gmail.com>
2021-05-06 20:15:17 +0200

    Remove unnecessary ternary operator

--- src/commands/periodic.ts
+++ src/commands/periodic.ts
@@ -15,7 +15,7 @@ async function command(msg: Discord.Message) {
         data = await db.insertData({ id: msg.author.id, portfolio: {} }, COLLECTIONS.CRYPTO, true);
     }
 
-    data.periodic = !data.periodic ? true : false;
+    data.periodic.state = !data.periodic;
     await db.updatePartialData(data._id, { periodic: data.periodic }, COLLECTIONS.CRYPTO);
     msg.reply(`Enable Periodic Updates: ${data.periodic}`);
     msg.delete();
